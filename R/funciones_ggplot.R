e111d29f654e39ff0f00a1310f8a24bd34b9179b
smendeza1 <smendez@axoncg.com>
2017-02-07 21:16:18 -0600

    Agregar mensaje de bienvenida

--- R/funciones_ggplot.R
+++ R/funciones_ggplot.R
@@ -1,16 +1,21 @@
+.onAttach <- function(libname, pkgname) { packageStartupMessage("Paquete de utilidades Axon 0.1.0")
+}
+
+
+
 ggaxon <- function(grafico, fuente) {
     require(png)
     require(grid)
     require(gridExtra)
-    
+
     img <- readPNG(source = "man/axon.png")
-    
+
     texto <- paste0("Fuente: ", fuente)
-    my_g <- grobTree(rectGrob(gp = gpar(fill = "white", col = "white")), textGrob(texto, x = 0.99, hjust = 1, vjust = 0.5, 
+    my_g <- grobTree(rectGrob(gp = gpar(fill = "white", col = "white")), textGrob(texto, x = 0.99, hjust = 1, vjust = 0.5,
         gp = gpar(col = "#737373", fontfamily = "Open Sans", fontsize = 9)), rasterGrob(img, x = 0.01, hjust = 0))
-    
+
     grid.arrange(grafico, my_g, heights = c(1, 0.05))
-    
+
 }
 
 
@@ -22,12 +27,12 @@ theme_axon <- function(base_family = "Open Sans", base_size = 11) {
     color.axis.title = palette[7]
     color.title = palette[9]
     color.axis = palette[5]
-    theme_bw(base_size = base_size, base_family = base_family) + theme(panel.border = element_rect(color = color.background), 
-        panel.grid.major = element_line(color = color.grid.major, size = 0.25, linetype = 2), panel.grid.minor = element_blank(), 
-        axis.line.x = element_line(color = color.axis), axis.line.y = element_line(color = color.axis), axis.ticks = element_line(color = color.axis), 
-        legend.key = element_rect(fill = NA, color = NA), legend.text = element_text(size = rel(0.8), color = color.axis.title), 
-        plot.title = element_text(color = color.axis.title, size = rel(1.2), vjust = 1.25), axis.text.x = element_text(size = rel(0.9), 
-            color = color.axis.text), axis.text.y = element_text(size = rel(0.9), color = color.axis.text), axis.title.x = element_text(size = rel(1), 
-            color = color.axis.title, vjust = 0), axis.title.y = element_text(size = rel(1), color = color.axis.title, 
+    theme_bw(base_size = base_size, base_family = base_family) + theme(panel.border = element_rect(color = color.background),
+        panel.grid.major = element_line(color = color.grid.major, size = 0.25, linetype = 2), panel.grid.minor = element_blank(),
+        axis.line.x = element_line(color = color.axis), axis.line.y = element_line(color = color.axis), axis.ticks = element_line(color = color.axis),
+        legend.key = element_rect(fill = NA, color = NA), legend.text = element_text(size = rel(0.8), color = color.axis.title),
+        plot.title = element_text(color = color.axis.title, size = rel(1.2), vjust = 1.25), axis.text.x = element_text(size = rel(0.9),
+            color = color.axis.text), axis.text.y = element_text(size = rel(0.9), color = color.axis.text), axis.title.x = element_text(size = rel(1),
+            color = color.axis.title, vjust = 0), axis.title.y = element_text(size = rel(1), color = color.axis.title,
             vjust = 1.25), plot.subtitle = element_text(color = color.axis.title), plot.caption = element_text(color = color.axis.title))
 }
